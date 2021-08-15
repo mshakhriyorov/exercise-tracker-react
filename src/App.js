ccfee8d6112ce0b47b68cf6cbdbf98929b915efd
Mukhriddin Shakhriyorov <71601329+mshakhriyorov@users.noreply.github.com>
2021-08-15 17:41:24 +0500

    for-bg-img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+// import bg from "./assets/cold-bg.jpg";
+
+const api = {
+  key: "7d3b537b1c55a7b72bee86d86c434b58",
+  base: "https://home.openweathermap.org/data/2.5"
+};
 
 function App() {
   return (
-    <div className="App">
-      <h1>Go React</h1>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+      </main>
     </div>
   );
 }
